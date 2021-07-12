be76cd9a1f5d0560741f1f9fdd27737734f0dc18
Joe Farned <JFarned@insightpartners.com>
2021-07-12 13:33:20 -0400

    update homepage

---
+++ build/precache-manifest.2e156723418fec08a4d208132bf6689f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee38440ff3c373b9b4ba3626806539e",
+    "url": "/monday-app/index.html"
+  },
+  {
+    "revision": "33f771b7ebd223b0a04e",
+    "url": "/monday-app/static/css/2.3e71eac9.chunk.css"
+  },
+  {
+    "revision": "61ee46127a17cdcf3a60",
+    "url": "/monday-app/static/css/main.4fd7d449.chunk.css"
+  },
+  {
+    "revision": "33f771b7ebd223b0a04e",
+    "url": "/monday-app/static/js/2.bf96c7ce.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/monday-app/static/js/2.bf96c7ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ee46127a17cdcf3a60",
+    "url": "/monday-app/static/js/main.e82f6a5d.chunk.js"
+  },
+  {
+    "revision": "4a40a92adbc6eb6f6c60",
+    "url": "/monday-app/static/js/runtime-main.8e015eaa.js"
+  }
+]);
